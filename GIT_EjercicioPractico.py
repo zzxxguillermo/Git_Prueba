ce5050e6b479f573d57c1c202da6094d132a7fef
zzxxguillermo <71794189+zzxxguillermo@users.noreply.github.com>
2026-05-04 15:06:58 -0300

    Formulario Agregado - Otro archivo nuevo

---
+++ GIT_EjercicioPractico.py
@@ -0,0 +1,15 @@
+numero = int(input("Ingresá un número: "))
+
+if numero != 0:
+    if numero > 0:
+        if numero % 2 == 0:
+            print("El número es distinto de 0, positivo y par")
+        else:
+            print("El número es positivo pero impar")
+    else:
+        print("El número es distinto de 0 pero negativo")
+else:
+    print("El número es 0")
+    # Mi primer codigo en GIT
+    
+    
